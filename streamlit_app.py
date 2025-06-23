60f27fbab62c866e97e3f9f9afa434e2a0f9082c
Aravindasai4 <102633769+Aravindasai4@users.noreply.github.com>
2025-06-23 17:27:54 +0000

    commiting after cah geing from line to candles

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,25 @@ if submitted:
         for ticker in tickers:
             st.markdown(f"### 📈 {ticker} Chart")
             st.components.v1.html(f"""
-            <div class="tradingview-widget-container">
-              <iframe src="https://s.tradingview.com/embed-widget/mini-symbol-overview/?locale=en#%7B%22symbol%22%3A%22NASDAQ%3A{ticker}%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A300%2C%22locale%22%3A%22en%22%2C%22dateRange%22%3A%22ytd%22%2C%22colorTheme%22%3A%22dark%22%2C%22trendLineColor%22%3A%22rgba(41%2C%2098%2C%20255%2C%201)%22%2C%22underLineColor%22%3A%22rgba(41%2C%2098%2C%20255%2C%200.3)%22%2C%22isTransparent%22%3Afalse%2C%22autosize%22%3Atrue%7D" 
-              width="100%" height="300" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
-            </div>
-            """, height=320)
+<div class="tradingview-widget-container" style="margin-top:10px">
+  <div id="tradingview_{ticker}" />
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+    new TradingView.widget({{
+      "width": "100%",
+      "height": 500,
+      "symbol": "NASDAQ:{ticker}",
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "dark",
+      "style": "1",  // Candlestick style
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "allow_symbol_change": false,
+      "container_id": "tradingview_{ticker}"
+    }});
+  </script>
+</div>
+""", height=520)
+
