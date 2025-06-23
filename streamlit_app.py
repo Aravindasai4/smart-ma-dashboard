1753e93c677de03b4e0b257c183ce1db9edfebea
Aravindasai4 <102633769+Aravindasai4@users.noreply.github.com>
2025-06-23 17:33:37 +0000

    added 20 day 200 day and candle stick

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,25 +18,31 @@ if submitted:
         for ticker in tickers:
             st.markdown(f"### 📈 {ticker} Chart")
             st.components.v1.html(f"""
-<div class="tradingview-widget-container" style="margin-top:10px">
-  <div id="tradingview_{ticker}" />
+<div class="tradingview-widget-container" style="margin-top: 20px">
+  <div id="tradingview_{ticker}"></div>
   <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
   <script type="text/javascript">
     new TradingView.widget({{
+      "container_id": "tradingview_{ticker}",
       "width": "100%",
       "height": 500,
       "symbol": "NASDAQ:{ticker}",
       "interval": "D",
       "timezone": "Etc/UTC",
       "theme": "dark",
-      "style": "1",  // Candlestick style
+      "style": "1",  // Candlestick
       "locale": "en",
       "toolbar_bg": "#f1f3f6",
       "enable_publishing": false,
-      "allow_symbol_change": false,
-      "container_id": "tradingview_{ticker}"
+      "hide_side_toolbar": false,
+      "studies": ["MAExp@tv-basicstudies", "MAExp@tv-basicstudies"],
+      "study_config": {{
+        "MAExp@tv-basicstudies": [{{"length":20}}, {{"length":200}}]
+      }},
+      "allow_symbol_change": false
     }});
   </script>
 </div>
 """, height=520)
+
 
